074358b666efad76c22ad08a54cfcff358ecaa35
zacharyae <elliottzachary1st@yahoo.com>
2024-05-25 21:19:50 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ procList = ["living christ", "the family", "family proclamation", "restoration p
 
 book_name_map = {"Genesis": "gen", "Exodus": "ex", "Leviticus": "lev", "Numbers": "num", "Deuteronomy": "deut", "Joshua": "josh", "Judges": "judg", "Ruth": "ruth", "1 Samuel": "1-sam", "2 Samuel": "2-sam", "1 Kings": "1-kgs", "2 Kings": "2-kgs", "1 Chronicles": "1-chr", "2 Chronicles": "2-chr", "Ezra": "ezra", "Nehemiah": "neh", "Esther": "esth", "Job": "job", "Psalms": "ps", "Proverbs": "prov", "Ecclesiastes": "eccl", "Song of Solomon": "song", "Isaiah": "isa", "Jeremiah": "jer", "Lamentations": "lam", "Ezekiel": "ezek", "Daniel": "dan", "Hosea": "hosea", "Joel": "joel", "Amos": "amos", "Obadiah": "obad", "Jonah": "jonah", "Micah": "micah", "Nahum": "nahum", "Habakkuk": "hab", "Zephaniah": "zeph", "Haggai": "hag", "Zechariah": "zech", "Malachi": "mal", "Matthew": "matt", "Mark": "mark", "Luke": "luke", "John": "john", "Acts": "acts", "Romans": "rom", "1 Corinthians": "1-cor", "2 Corinthians": "2-cor", "Galatians": "gal", "Ephesians": "eph", "Philippians": "philip", "Colossians": "col", "1 Thessalonians": "1-thes", "2 Thessalonians": "2-thes", "1 Timothy": "1-tim", "2 Timothy": "2-tim", "Titus": "titus", "Philemon": "philem", "Hebrews": "heb", "James": "james", "1 Peter": "1-pet", "2 Peter": "2-pet", "1 John": "1-jn", "2 John": "2-jn", "3 John": "3-jn", "Jude": "jude", "Revelation": "rev", "1 Nephi": "1-ne", "2 Nephi": "2-ne", "Jacob": "jacob", "Enos": "enos", "Jarom": "jarom", "Omni": "omni", "Words Of Mormon": "w-of-m", "Mosiah": "mosiah", "Alma": "alma", "Helaman": "hel", "3 Nephi": "3-ne", "4 Nephi": "4-ne", "Mormon": "morm", "Ether": "ether", "Moroni": "moro", "Moses": "moses", "Abraham": "abr", "Joseph Smith—Matthew": "js-m", "Joseph Smith—History": "js-h", "Articles Of Faith": "a-of-f"}
 
-MAX_EMBED_LENGTH = 1000
-MAX_FIELD_LENGTH = 600
+MAX_EMBED_LENGTH = 1200
+MAX_FIELD_LENGTH = 1024
 
 def format_scripture_message(book, chapterNumber, verses, page_num, items_per_page):
     print(verses)
